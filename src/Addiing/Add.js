268cbba236d2d77d4dc631d62494f0c5a4d9f4e3
Srikar-C <csrikar2003@gmail.com>
2024-05-10 20:25:15 +0530

    final commit

--- src/Addiing/Add.js
+++ src/Addiing/Add.js
@@ -1,82 +1,105 @@
-import { useState } from "react";
-import "./add.css";
-import Item from "./Item";
+import { useEffect, useState } from 'react'
+import './add.css'
+import Item from './Item'
 
 function Add() {
   function getDate() {
-    const today = new Date();
-    const month = today.getMonth() + 1;
-    const year = today.getFullYear();
-    const date = today.getDate();
-    return `${year}-${month}-${date}`;
+    const today = new Date()
+    const month = today.getMonth() + 1
+    const year = today.getFullYear()
+    const date = today.getDate()
+    return `${year}-${month}-${date}`
   }
-  const [time,setTime] = useState((new Date).getHours()+":"+(new Date()).getMinutes());
-  const [date, setDate] = useState(getDate());
-  
+
+  const [date, setDate] = useState(getDate())
+
   function handleDate(event) {
-    setDate(event.target.value);
+    setDate(event.target.value)
   }
-  const [text, setText] = useState("");
+  const [text, setText] = useState('')
   function handleText(event) {
-    setText(event.target.value);
+    setText(event.target.value)
   }
-  const [data, setData] = useState([]);
-  const [dupData,setDupdata] = useState([]);
+  const [data, setData] = useState([])
   function handleData(event) {
-    event.preventDefault();
+    event.preventDefault()
     setData((prevData) => {
-      if(text===""){
-        return [...prevData,date+" => " + time+ " => Nothing to do."];
+      if (text === '') {
+        return [...prevData, date + ' => Nothing to do.']
+      } else {
+        return [...prevData, date + ' => ' + text]
       }
-      else {
-        return [...prevData,date+" => " + time+ " => "+text];
-      }  
-    });
-    setDupData((prevData) => {
-      if(text===""){
-        return [...prevData,date+" => " + time+ " => Nothing to do."];
-      }
-      else {
-        return [...prevData,date+" => " + time+ " => "+text];
-      }  
-    });
-    setDate(getDate());
-    setText("");
+    })
+    setDate(getDate())
+    setText('')
+
+    console.log(text, date)
+
+    fetch('http://localhost:3005/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: text, date: date }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) =>
+        console.error('There was a problem with the fetch operation:', error)
+      )
   }
   function onDelete(id) {
-    console.log(id);
-    setData((prevData)=>{
-      return prevData.filter((item, key) => {
-        console.log("loop: "+key+":"+item);
-        return index !== id;
-      });
-    });
+    console.log(id)
+    setData((prevData) => {
+      return prevData.filter((item, index) => {
+        return index !== id
+      })
+    })
   }
-  function handleTime(event){
-    setTime(event.target.value);
-  }
-  
+
+  useEffect(() => {
+    fetch('http://localhost:3005/lists', {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data1) => {
+        console.log(data1)
+        data1.forEach((item) => {
+          setData((prevData) => {
+            return [...prevData, '' + item.date + ' => ' + item.itemName]
+          })
+        })
+      })
+      .catch((error) =>
+        console.error('There was a problem with the fetch operation:', error)
+      )
+  }, [])
+
+  console.log('data ', data)
   return (
     <div className="container">
       <h1>TO DO LIST</h1>
       <form className="form">
         <input type="date" onChange={handleDate} value={date} />
-        <input type="time" onChange={handleTime} value={time}/>
         <input
           type="text"
           onChange={handleText}
           value={text}
           placeholder="Enter text"
-          style={{ color: "white" }}
-        />        
+          style={{ color: 'white' }}
+        />
         <button onClick={handleData}>Add</button>
       </form>
       <div className="list">
-        {data.map((item, index) => (
-          <Item key={index} id={index} value={item} onChecked={onDelete} />
-        ))}
+        {data.length > 0 &&
+          data.map((item, index) => {
+            console.log('item ', item)
+            return (
+              <Item key={index} id={index} val={item} onChecked={onDelete} />
+            )
+          })}
       </div>
     </div>
-  );
+  )
 }
-export default Add;
+export default Add
--- src/Addiing/Item.js
+++ src/Addiing/Item.js
@@ -1,31 +1,16 @@
-import { useState } from "react";
-import "./item.css";
+import './item.css'
 export default function Item(props) {
-  const [newval,setNewVal] = useState(props.value);
-  const [toggle,setToggle] = useState(false);
-  function handleUpdate(){
-    var val=prompt("Enter new value");
-    let str = props.value;
-    let s = str.split(" => ");
-    s[2] = val;
-    setNewVal(s[0]+" => "+s[1]+" => "+s[2]);
-    // set
-    
-    // setToggle(!toggle);
-  }
-  const [val,setValue] = useState(props.value);
   return (
     <div className="item-list">
       <h3>{props.id}.</h3>
       <h3>{props.val}</h3>
       <button
         onClick={() => {
-          props.onChecked(props.id);
+          props.onChecked(props.id)
         }}
       >
         Delete
       </button>
-      <button onClick={handleUpdate}>Update</button>
     </div>
-  );
+  )
 }
